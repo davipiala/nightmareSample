ff9bceb94c63e75c8c137a88df5596e569fae841
FILES=0 <files.eq.zero@gmail.com>
2017-09-20 23:45:04 +0900

    add hi&low logic

--- sample4ConditionAndLoop.js
+++ sample4ConditionAndLoop.js
@@ -1,20 +1,47 @@
 const Nightmare = require('nightmare'); // import から requreに変更
-const nightmare = Nightmare({ show: true });
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
+const vo = require('vo');
 
-nightmare
-    .goto('http://localhost:8000/angularsample2.html')
-    .wait('#expect')  //追加
-    .type('#expect', '10')
-    .click('#doExpect')
-    .evaluate(() => {
-        return document.getElementsByTagName('body')[0].innerHTML;
-    })
-    .end()
-    .then((doc) => {
+function gotoHighLow(nm){
+    return nm
+        .goto('http://localhost:8000/angularsample2.html')
+        .wait('#expect');
+}
+
+function doExpect(nm, num){
+    return nm
+        .type('#expect')
+        .type('#expect', num)
+        .click('#doExpect')
+        .evaluate(() => {
+            return document.getElementsByTagName('body')[0].innerHTML;
+        });
+}
+
+vo(function* () {
+    let nightmare = Nightmare({ show: true });
+    yield gotoHighLow(nightmare);
+    let min=1,max=100;
+    let hint='';
+    let times='';
+    while(hint!='correct!'){
+        let expectedNum=Math.floor((min + max)/2);
+        let doc=yield doExpect(nightmare,expectedNum);
         const $ = cheerio.load(doc);
-        console.log($("#hint").text());
-    })
-    .catch((error) => {
-        console.error('Search failed:', error);
-    });
+        hint = $("#hint").text();
+        times= $("#times").text();
+        console.log(min + '<' + expectedNum + '<' + max + ':' + hint);
+        if( hint=='low'){
+            min=expectedNum;
+        } else if(hint=='high'){
+            max=expectedNum;
+        }
+    }
+
+
+    yield nightmare.end();
+    return times;
+})(function (err, result) {
+    if (err) return console.log(err);
+    console.log(result + "Times");
+});
